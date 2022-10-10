dcd1ca4661e81e5e978faefa799465515f2487dd
AndreiMironau <mistersmit1985@gmail.com>
2022-10-10 19:11:28 +0300

    Add new 3 lines

--- src/main/java/Java01.java
+++ src/main/java/Java01.java
@@ -1,5 +1,8 @@
 public class Java01 {
     public static void main(String[] args) {
         System.out.println("Hi");
+        System.out.println("Hi_1");
+        System.out.println("Hi_2");
+        System.out.println("Hi_3");
     }
 }
